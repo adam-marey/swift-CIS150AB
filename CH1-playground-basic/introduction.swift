97d4bd5ef6eb72c8d8fc5811fab8225404e4bbda
Sulaiman Marey <adammarey51@gmail.com>
2022-09-11 22:32:42 -0700

    added ch1 files

---
+++ CH1-playground-basic/introduction.swift
@@ -0,0 +1,11 @@
+//: ## Playground Basics
+//: Welcome to your first playground! In this course, youâ€™ll use playgrounds to learn the basics of programming. To get started, youâ€™ll take a short tour and write a bit of code.
+//:
+//: Before you start, do you see animating dots below?
+//: If you donâ€™t, go to the top menu bar and select Editor > Show Rendered Markup.
+//:
+//: ![animating dots](swiftloading.gif)
+//:
+//: If you can see the animating dots, youâ€™re ready to get started.
+//:
+//:page 1 of 7  |  [Next: What Is a Playground?](@next)
